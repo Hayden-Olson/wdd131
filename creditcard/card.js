2cc80eb890bbf0c8771fa533f7dca7647a81ea2e
Hayden Olson <haydenolson@byui.edu>
2025-06-06 16:46:19 -0600

    changes for card

--- creditcard/card.js
+++ creditcard/card.js
@@ -4,28 +4,39 @@ function isCardNumberValid(number) {
 }
 function displayError(msg) {
 	// display error message
-	document.querySelector('.errorMsg').innerHTML = msg
+	const errorElement = document.querySelector('.errorMsg')
+	errorElement.hidden = false
+	errorElement.innerText = msg
+}
+function isDateInFuture(year, month) {
+	return new Date() < new Date(year, month + 1, 0)
 }
 function submitHandler(event) {
 	event.preventDefault()
 	let errorMsg = ''
-	console.log(this.cardNumber.value)
+	const value = new FormData(event.target)
+	console.log(event.currentTarget)
 	// clear any previous errors
 	displayError('')
 	// check credit card number
-	if (isNaN(this.cardNumber.value)) {
+	if (isNaN((value.get('cardNumber')))) {
 		// it is not a valid number
 		errorMsg += 'Card number is not a valid number\n'
-	} else if (!isCardNumberValid(this.cardNumber.value)) {
+	} else if (!isCardNumberValid(value.get('cardNumber'))) {
 		// it is a number, but is it valid?
 		errorMsg += 'Card number is not a valid card number\n'
+	}
+	if (isDateInFuture((value.get('year')),+ (value.get('month'))) === false) {
+		errorMsg += 'Expired Card\n'
 	}
 	if (errorMsg !== '') {
 		// there was an error. stop the form and display the errors.
 		displayError(errorMsg)
 		return false
 	}
+	console.log('success')
 	return true
+
 }
 
 document.querySelector('#credit-card').addEventListener('submit', submitHandler)
