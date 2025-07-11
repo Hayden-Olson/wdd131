c16af2961872bc6886517febfdac04e43d604017
Hayden Olson <haydenolson@byui.edu>
2025-07-11 03:06:15 -0600

    java script added and also birdy

---
+++ final/index.js
@@ -0,0 +1,31 @@
+const characters = [
+    {
+
+    }
+]
+
+const enemies = [
+    {
+        number: 1,
+        name: 'Shot',
+        image: './pictures/Shot.jpg',
+        health: 1,
+        defense: 0,
+        location: 'Cannon Canyon',
+        tags: ['Normal', 'Creature', 'Enemy', 'Shot'],
+        description: 'Cannon shots fired by Cannon Troopers that came alive for some reason. They are very simple creatures that usually try to fend off others by bonking into them.'
+    },
+    {
+        number: 2,
+        name: "Bowler Shot",
+        image: './pictures/Bowler_Shot.jpg',
+        health: 1,
+        defense: 4,
+        location: "Cannon Castle",
+        tags: ['Steel', 'Creature', 'Enemy', 'Shot'],
+        description: 'The ironclad variant of a Shot. Due to their immense weight, they struggle to move around. They also can no longer jump, so they specialize in rolling. Countering is a great way to handle this foe. They also come in many different colors!'
+    },
+    {
+        
+    }
+]
